f97bc6cdc5631c26d6d9cef146b8cdfbc35fe91f
Meyer McMains <meyer.mcmains@gmail.com>
2017-04-27 16:25:04 -0400

    Play Us Out? What Does That Even Mean?
    To End The Show?

--- js/directory.js
+++ js/directory.js
@@ -1,18 +1,20 @@
 $('.faculty').click(function () {
-	$('.desc').toggle(200);
-	if ($('.carrot').data('carrot') === true) {
-		$('.carrot').css({'-webkit-transform' : 'rotate('+ 0 +'deg)',
+	var person = $(this).children('.desc');
+	var carrot = $(this).find('.carrot');
+	$(person).toggle(200);
+	if ($(carrot).data('carrot') === true) {
+		$(carrot).css({'-webkit-transform' : 'rotate('+ 0 +'deg)',
                  '-moz-transform' : 'rotate('+ 0 +'deg)',
                  '-ms-transform' : 'rotate('+ 0 +'deg)',
                  'transform' : 'rotate('+ 0 +'deg)'});
-		$('.carrot').data('carrot', false);
+		$(carrot).data('carrot', false);
 	}
 	else {
-		$('.carrot').css({'-webkit-transform' : 'rotate('+ 90 +'deg)',
+		$(carrot).css({'-webkit-transform' : 'rotate('+ 90 +'deg)',
                  '-moz-transform' : 'rotate('+ 90 +'deg)',
                  '-ms-transform' : 'rotate('+ 90 +'deg)',
                  'transform' : 'rotate('+ 90 +'deg)'});
-		$('.carrot').data('carrot', true);
+		$(carrot).data('carrot', true);
 	}
 
 });
